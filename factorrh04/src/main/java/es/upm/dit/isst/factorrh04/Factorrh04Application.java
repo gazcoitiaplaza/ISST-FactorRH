4c2324d8f6e056b32a2450dae2cd83823ecc28cd
Guillermo Azcoitia <gazcoitiaplaza@gmail.com>
2024-04-25 16:18:56 +0200

    feat: add initial project structure

---
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/Factorrh04Application.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.factorrh04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Factorrh04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Factorrh04Application.class, args);
+	}
+
+}
---
+++ factorrh04/src/test/java/es/upm/dit/isst/factorrh04/Factorrh04ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.factorrh04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Factorrh04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
