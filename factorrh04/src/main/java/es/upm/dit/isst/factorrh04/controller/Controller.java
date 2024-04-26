c26a5e339016daf86b2e6aaf02b74be9bd513437
Juan Torres Babe <juanjuang033@gmail.com>
2024-04-26 16:43:52 +0200

    feat: foto RH y getmapping del controlador del log

--- factorrh04/src/main/java/es/upm/dit/isst/factorrh04/controller/Controller.java
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/controller/Controller.java
@@ -1,5 +1,11 @@
 package es.upm.dit.isst.factorrh04.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class Controller {
-    
+    @GetMapping("/")
+    public String inicio() {
+        return "login.html";
+    }
+  
 }
