a990413aed2259593c4b46d8dcf0e207eda2c416
Pablo y Alejandro <a.arrunada@alumnos.upm.es>
2024-04-28 10:26:16 +0200

    ok

--- factorrh04/src/main/java/es/upm/dit/isst/factorrh04/controller/LoginController.java
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/controller/LoginController.java
@@ -1,13 +1,1 @@
-package es.upm.dit.isst.factorrh04.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class LoginController {
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";  // Devuelve la vista del formulario de inicio de sesión personalizado
-    }
-}
