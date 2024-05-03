f8e7d8a6632c6f50722d2336fce93610cf09472a
Alvaro <a.colin@alumnos.upm.es>
2024-05-03 21:29:25 +0200

    login cosas hechas, guardar por si acaso

--- factorrh04/src/main/java/es/upm/dit/isst/factorrh04/security/SecurityConfig.java
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/security/SecurityConfig.java
@@ -1,90 +1,61 @@
-// package es.upm.dit.isst.factorrh04.security;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
-
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http
-//                 .authorizeHttpRequests(auth -> {
-//                     auth.requestMatchers("/").permitAll();
-//                 })
-//                 .formLogin(form -> form.loginPage("/login").permitAll());
-//         return http.build();
-//     }
-// }
-
 package es.upm.dit.isst.factorrh04.security;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    //PREGUNTAR LO DEL PASSWORDENCONDER
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    //     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    // }
+    @Autowired
+    private DataSource dataSource;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-    // @Bean
-    // //Configuracion lectura de credenciales desde esquema estandar
-    // public UserDetailsService jbdcUserDetailsService(DataSource dataSource, PasswordEncoder passwordEncoder) {
-    //     String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
-    //     String authsByUserQuery = "select username, authority from authorities where username = ?";
-    //     JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-    //     users.setUsersByUsernameQuery(usersByUsernameQuery);
-    //     users.setAuthoritiesByUsernameQuery(authsByUserQuery);
-    //     return users;
-    // }
+    @Bean
+    public UserDetailsService jbdcUserDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "SELECT id_Empleado as username, password, true FROM Empleado WHERE id_Empleado = ?";
+        String authsByUsernameQuery = "SELECT id_Empleado as username, puesto as authority FROM authorities WHERE id_Empleado = ?";
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.setUsersByUsernameQuery(usersByUsernameQuery);
+        users.setAuthoritiesByUsernameQuery(authsByUsernameQuery);
+        return users;
+    }
 
-
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({ "deprecation", "removal" })
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> {
-                    csrf.disable();
-                })
-                .authorizeRequests(authorizeRequests -> {
-                    authorizeRequests
-                            .requestMatchers("/login").permitAll()
-                            .requestMatchers("/static/**").permitAll()
-                            .anyRequest().authenticated();
-                })
-                
-                .formLogin(form -> form
-                                .loginPage("/login") // Especificar la página de inicio de sesión personalizada
-                                .permitAll() // Permitir acceso a la página de inicio de sesión
-                                .defaultSuccessUrl("/", true) // Redirigir al usuario a home después de iniciar sesión
-                )
-                .logout(logout -> logout
-                                .logoutSuccessUrl("/login?logout")
-                                .permitAll()
-                )
-                .exceptionHandling(handling -> handling.accessDeniedPage("/login"));
+            .authorizeRequests(requests -> requests
+                    .requestMatchers("/login", "/static/**", "h2-console/**").permitAll()
+                    .anyRequest().permitAll()
+            )
+            .formLogin(login -> login
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+            )
+            .logout(logout -> logout
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+            )
+            .exceptionHandling(handling -> handling
+                    .accessDeniedPage("/login")
+            )
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions().disable());
 
-            //Deshabilitamos proteccion X-Frame-Options
-            http.headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
         return http.build();
     }
 }
--- factorrh04/src/main/resources/data.sql
+++ factorrh04/src/main/resources/data.sql
@@ -1,9 +1,14 @@
-
-
 -- Inserción de datos de ejemplo en la tabla Empleado
-INSERT INTO Empleado (id_Empleado, nombre, apellido_1, apellido_2, email, IBAN, password, puesto) VALUES
-('0001', 'Alvaro', 'Colin', 'Cosano', 'juan.perez@example.com', 'ES6600190020961234567890', 'pass1234', 'EMP'),
-('0002', 'Guillermo', 'Azcoitia', 'Plaza', 'maria.lopez@example.com', 'ES6600190020962345678901', 'pass1234', 'EMP'),
-('0003', 'Andres', 'Gil', 'Oliva', 'carlos.fernandez@example.com', 'ES6600190020963456789012', 'pass1234', 'EMP'),
-('0004', 'Juan', 'Torres', 'Babe', 'lucia.garcia@example.com', 'ES6600190020964567890123', 'pass1234', 'EMP'),
-('0005', 'David', 'Martinez', 'Garcia', 'david.martinez@example.com', 'ES6600190020965678901234', 'pass1234', 'RH');
+INSERT INTO Empleado (id_Empleado, nombre, apellido_1, apellido_2, email, IBAN, password) VALUES
+('0001', 'Alvaro', 'Colin', 'Cosano', 'juan.perez@example.com', 'ES6600190020961234567890', 'pass1234'),
+('0002', 'Guillermo', 'Azcoitia', 'Plaza', 'maria.lopez@example.com', 'ES6600190020962345678901', 'pass1234'),
+('0003', 'Andres', 'Gil', 'Oliva', 'carlos.fernandez@example.com', 'ES6600190020963456789012', 'pass1234'),
+('0004', 'Juan', 'Torres', 'Babe', 'lucia.garcia@example.com', 'ES6600190020964567890123', 'pass1234'),
+('0005', 'David', 'Martinez', 'Garcia', 'david.martinez@example.com', 'ES6600190020965678901234', 'pass1234');
+
+INSERT INTO authorities (id_Empleado, puesto) VALUES
+('0001', 'ROLE_EMP'),
+('0002', 'ROLE_EMP'),
+('0003', 'ROLE_EMP'),
+('0004', 'ROLE_EMP'),
+('0005', 'ROLE_RH');
