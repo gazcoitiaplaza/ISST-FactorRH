d78a66ab4b761fcb99e081a065c730cdf1ed54f1
Alvaro <a.colin@alumnos.upm.es>
2024-05-03 18:15:26 +0200

    sql

--- factorrh04/src/main/resources/data.sql
+++ factorrh04/src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO Empleado (id_Empleado, nombre, apellido_1, apellido_2, email, IBAN, 
 ('0001', 'Alvaro', 'Colin', 'Cosano', 'juan.perez@example.com', 'ES6600190020961234567890', 'pass1234', 'EMP'),
 ('0002', 'Guillermo', 'Azcoitia', 'Plaza', 'maria.lopez@example.com', 'ES6600190020962345678901', 'pass1234', 'EMP'),
 ('0003', 'Andres', 'Gil', 'Oliva', 'carlos.fernandez@example.com', 'ES6600190020963456789012', 'pass1234', 'EMP'),
-('0004', 'LJuan', 'Torres', 'Babe', 'lucia.garcia@example.com', 'ES6600190020964567890123', 'pass1234', 'EMP'),
+('0004', 'Juan', 'Torres', 'Babe', 'lucia.garcia@example.com', 'ES6600190020964567890123', 'pass1234', 'EMP'),
 ('0005', 'David', 'Martinez', 'Garcia', 'david.martinez@example.com', 'ES6600190020965678901234', 'pass1234', 'RH');
