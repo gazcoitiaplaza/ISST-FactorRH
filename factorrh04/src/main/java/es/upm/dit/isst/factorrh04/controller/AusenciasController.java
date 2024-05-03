48341e24627d4460d70a25bdf3bd4b0e4adbb155
Alvaro <a.colin@alumnos.upm.es>
2024-05-03 13:12:39 +0200

    debug ausenciascontroller

--- factorrh04/src/main/java/es/upm/dit/isst/factorrh04/controller/AusenciasController.java
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/controller/AusenciasController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate; // Importa LocalDate
+
 import java.util.List;
 
 @Controller
