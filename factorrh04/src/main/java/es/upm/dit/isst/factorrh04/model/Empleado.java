e5aba9c12c792b8d38675922d346cc76329cf88e
Guillermo Azcoitia <gazcoitiaplaza@gmail.com>
2024-05-03 17:45:55 +0200

    feat: changed Empleado.java

--- factorrh04/src/main/java/es/upm/dit/isst/factorrh04/model/Empleado.java
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/model/Empleado.java
@@ -22,35 +22,10 @@ public class Empleado {
     private String apellido_1;
     @Column(name = "apellido_2", length = 40)
     private String apellido_2;
-    @Column(name = "NIF", length = 9)
-    private String NIF;
-    @Column(name = "NASS", length = 12)
-    private String NASS;
-    private String direccion;
-    @Column(name = "CP", length = 5)
-    private String CP;
-    @Column(name = "email_particular", length = 50)
-    private String email_particular;
     @Column(name = "email", length = 50)
     private String email;
-    @Column(name = "telefono", length = 12)
-    private String telefono;
-    @Temporal(TemporalType.DATE)
-    // Wed Mar 27 08:22:02 IST 2015
-    @Column(name = "fecha_alta", nullable = true, columnDefinition = "DATE DEFAULT CURRENT_DATE")
-    private Date fecha_alta;
-    @Temporal(TemporalType.DATE)
-    @Column(name = "fecha_baja", nullable = true, columnDefinition = "DATE DEFAULT null")
-    private Date fecha_baja;
     @Column(name = "IBAN", length = 24)
     private String IBAN;
-    @Column(name = "SWIFT", length = 11)
-    private String SWIFT;
-    @Column(name = "sueldo_base", precision = 8, scale = 2)
-    private int sueldo_base;
-    @Column(name = "antiguedad", precision = 8, scale = 2)
-    private int antiguedad;
-    // private final byte[] foto;
     @Column(name = "password", length = 50)
     private String password;
     @Column(name = "puesto", length = 5)
@@ -99,47 +74,6 @@ public class Empleado {
         this.apellido_2 = apellido_2;
     }
 
-    public String getNIF() {
-        return NIF;
-    }
-
-    public void setNIF(String nIF) {
-        NIF = nIF;
-    }
-
-    public String getNASS() {
-        return NASS;
-    }
-
-    public void setNASS(String nASS) {
-        NASS = nASS;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getCP() {
-        return CP;
-    }
-
-    public void setCP(String cP) {
-        CP = cP;
-    }
-
-    //Quitar esto
-    public String getEmail_particular() {
-        return email_particular;
-    }
-
-    public void setEmail_particular(String email_particular) {
-        this.email_particular = email_particular;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -148,29 +82,6 @@ public class Empleado {
         this.email = email;
     }
 
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public Date getFecha_alta() {
-        return fecha_alta;
-    }
-
-    public void setFecha_alta(Date fecha_alta) {
-        this.fecha_alta = fecha_alta;
-    }
-
-    public Date getFecha_baja() {
-        return fecha_baja;
-    }
-
-    public void setFecha_baja(Date fecha_baja) {
-        this.fecha_baja = fecha_baja;
-    }
 
     public String getIBAN() {
         return IBAN;
@@ -180,29 +91,6 @@ public class Empleado {
         IBAN = iBAN;
     }
 
-    public String getSWIFT() {
-        return SWIFT;
-    }
-
-    public void setSWIFT(String sWIFT) {
-        SWIFT = sWIFT;
-    }
-
-    public int getSueldo_base() {
-        return sueldo_base;
-    }
-
-    public void setSueldo_base(int sueldo_base) {
-        this.sueldo_base = sueldo_base;
-    }
-
-    public int getAntiguedad() {
-        return antiguedad;
-    }
-
-    public void setAntiguedad(int antiguedad) {
-        this.antiguedad = antiguedad;
-    }
 
     public String getPassword() {
         return password;
