5a2a6cc5a87812b2941983f96fb0b6f2f736ed4b
Juan Torres Babe <juanjuang033@gmail.com>
2024-05-05 20:07:01 +0200

    feat: clases de baja

---
+++ factorrh04/src/main/java/es/upm/dit/isst/factorrh04/repository/BajasRepository.java
@@ -0,0 +1,12 @@
+package es.upm.dit.isst.factorrh04.repository;
+
+import es.upm.dit.isst.factorrh04.model.Ausencias;
+import es.upm.dit.isst.factorrh04.model.Bajas;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BajasRepository extends CrudRepository<Bajas, Long> {
+    List<Bajas> findByNombreUsuario(String nombreUsuario);
+    List<Bajas> findAll();
+}
